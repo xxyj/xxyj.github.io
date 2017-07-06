40a16fdfd9ad5d41213dd7cff850b0ed56d31762
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2017-07-06 20:41:45 +0800

    Site updated: 2017-07-06 20:41:44

--- js/common.js
+++ js/common.js
@@ -1,40 +1,37 @@
-(function() {
-
-  initMobileMenu()
+(function () {
+  initMobileMenu();
   if (PAGE_TYPE) {
-    initSubHeaders()
-    initLocationHashFuzzyMatching()
+    initSubHeaders();
+    initLocationHashFuzzyMatching();
   }
 
   function initLocationHashFuzzyMatching() {
-    var hash = window.location.hash
-    if (!hash) return
-    var hashTarget = document.getElementById(hash)
+    const hash = window.location.hash;
+    if (!hash) return;
+    const hashTarget = document.getElementById(hash);
     if (!hashTarget) {
-      var normalizedHash = normalizeHash(hash)
-      var possibleHashes = [].slice.call(document.querySelectorAll('[id]'))
-        .map(function(el) {
-          return el.id
-        })
-      possibleHashes.sort(function(hashA, hashB) {
-        var distanceA = levenshteinDistance(normalizedHash, normalizeHash(hashA))
-        var distanceB = levenshteinDistance(normalizedHash, normalizeHash(hashB))
-        if (distanceA < distanceB) return -1
-        if (distanceA > distanceB) return 1
-        return 0
-      })
-      window.location.hash = possibleHashes[0]
+      const normalizedHash = normalizeHash(hash);
+      const possibleHashes = [].slice.call(document.querySelectorAll('[id]'))
+        .map(el => el.id);
+      possibleHashes.sort((hashA, hashB) => {
+        const distanceA = levenshteinDistance(normalizedHash, normalizeHash(hashA));
+        const distanceB = levenshteinDistance(normalizedHash, normalizeHash(hashB));
+        if (distanceA < distanceB) return -1;
+        if (distanceA > distanceB) return 1;
+        return 0;
+      });
+      window.location.hash = possibleHashes[0];
     }
 
     function normalizeHash(rawHash) {
       return rawHash
         .toLowerCase()
-        .replace(/\-(?:deprecated|removed|replaced|changed|obsolete)$/, '')
+        .replace(/\-(?:deprecated|removed|replaced|changed|obsolete)$/, '');
     }
 
     function levenshteinDistance(a, b) {
-      var m = []
-      if (!(a && b)) return (b || a).length
+      const m = [];
+      if (!(a && b)) return (b || a).length;
       for (var i = 0; i <= b.length; m[i] = [i++]) {}
       for (var j = 0; j <= a.length; m[0][j] = j++) {}
       for (var i = 1; i <= b.length; i++) {
@@ -43,10 +40,10 @@
             ? m[i - 1][j - 1]
             : m[i][j] = Math.min(
               m[i - 1][j - 1] + 1,
-              Math.min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+              Math.min(m[i][j - 1] + 1, m[i - 1][j] + 1));
         }
       }
-      return m[b.length][a.length]
+      return m[b.length][a.length];
     }
   }
 
@@ -55,19 +52,19 @@
    */
 
   function initMobileMenu() {
-    var mobileBar = document.getElementById('mobile-bar')
-    var sidebar = document.querySelector('.sidebar')
-    var menuButton = mobileBar.querySelector('.menu-button')
+    const mobileBar = document.getElementById('mobile-bar');
+    const sidebar = document.querySelector('.sidebar');
+    const menuButton = mobileBar.querySelector('.menu-button');
 
-    menuButton.addEventListener('click', function() {
-      sidebar.classList.toggle('open')
-    })
+    menuButton.addEventListener('click', () => {
+      sidebar.classList.toggle('open');
+    });
 
-    document.body.addEventListener('click', function(e) {
+    document.body.addEventListener('click', (e) => {
       if (e.target !== menuButton && !sidebar.contains(e.target)) {
-        sidebar.classList.remove('open')
+        sidebar.classList.remove('open');
       }
-    })
+    });
   }
 
   /**
@@ -75,172 +72,156 @@
    */
 
   function initSubHeaders() {
-    var each = [].forEach
-    var main = document.getElementById('main')
-    var header = document.getElementById('header')
-    var sidebar = document.querySelector('.sidebar')
-    var content = document.querySelector('.content')
+    const each = [].forEach;
+    const main = document.getElementById('main');
+    const header = document.getElementById('header');
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
 
     // build sidebar
-    var currentPageAnchor = sidebar.querySelector('.sidebar-link.current')
-    var isAPI = document.querySelector('.content').classList.contains('api')
+    const currentPageAnchor = sidebar.querySelector('.sidebar-link.current');
+    const isAPI = document.querySelector('.content').classList.contains('api');
     if (currentPageAnchor || isAPI) {
-      var allHeaders = []
-      var sectionContainer
+      var allHeaders = [];
+      let sectionContainer;
       if (isAPI) {
-        sectionContainer = document.querySelector('.menu-root')
+        sectionContainer = document.querySelector('.menu-root');
       } else {
-        sectionContainer = document.createElement('ul')
-        sectionContainer.className = 'menu-sub'
-        currentPageAnchor.parentNode.appendChild(sectionContainer)
+        sectionContainer = document.createElement('ul');
+        sectionContainer.className = 'menu-sub';
+        currentPageAnchor.parentNode.appendChild(sectionContainer);
       }
-      var headers = content.querySelectorAll('h2')
+      let headers = content.querySelectorAll('h2');
       if (headers.length) {
-        each.call(headers, function(h) {
-          sectionContainer.appendChild(makeLink(h))
-          var h3s = collectH3s(h)
-          allHeaders.push(h)
-          allHeaders.push.apply(allHeaders, h3s)
-          if (h3s.length) {
-            sectionContainer.appendChild(makeSubLinks(h3s, isAPI))
-          }
-        })
+        each.call(headers, (h) => {
+          const h3s = collectH3s(h);
+          allHeaders.push(h);
+          allHeaders.push(...h3s);
+        });
       } else {
-        headers = content.querySelectorAll('h3')
-        each.call(headers, function(h) {
-          sectionContainer.appendChild(makeLink(h))
-          allHeaders.push(h)
-        })
+        headers = content.querySelectorAll('h3');
+        each.call(headers, (h) => {
+          allHeaders.push(h);
+        });
       }
 
-      var animating = false
-      sectionContainer.addEventListener('click', function(e) {
-        if (e.target.classList.contains('section-link')) {
-          sidebar.classList.remove('open')
-          setActive(e.target)
-          animating = true
-          setTimeout(function() {
-            animating = false
-          }, 400)
-        }
-      }, true)
+      var animating = false;
 
       // make links clickable
-      allHeaders.forEach(makeHeaderClickable)
+      allHeaders.forEach(makeHeaderClickable);
 
       smoothScroll.init({
         speed: 400,
-        offset: 0
-      })
+        offset: 0,
+      });
     }
 
-    var hoveredOverSidebar = false
-    sidebar.addEventListener('mouseover', function() {
-      hoveredOverSidebar = true
-    })
-    sidebar.addEventListener('mouseleave', function() {
-      hoveredOverSidebar = false
-    })
+    let hoveredOverSidebar = false;
+    sidebar.addEventListener('mouseover', () => {
+      hoveredOverSidebar = true;
+    });
+    sidebar.addEventListener('mouseleave', () => {
+      hoveredOverSidebar = false;
+    });
 
     // listen for scroll event to do positioning & highlights
-    window.addEventListener('scroll', updateSidebar)
-    window.addEventListener('resize', updateSidebar)
+    window.addEventListener('scroll', updateSidebar);
+    window.addEventListener('resize', updateSidebar);
 
     function updateSidebar() {
-      var doc = document.documentElement
-      var top = doc && doc.scrollTop || document.body.scrollTop
-      if (animating || !allHeaders) return
-      var last
-      for (var i = 0; i < allHeaders.length; i++) {
-        var link = allHeaders[i]
+      const doc = document.documentElement;
+      const top = doc && doc.scrollTop || document.body.scrollTop;
+      if (animating || !allHeaders) return;
+      let last;
+      for (let i = 0; i < allHeaders.length; i++) {
+        const link = allHeaders[i];
         if (link.offsetTop > top) {
-          if (!last) last = link
-          break
+          if (!last) last = link;
+          break;
         } else {
-          last = link
+          last = link;
         }
       }
-      if (last)
-        setActive(last.id, !hoveredOverSidebar)
+      if (last) { setActive(last.id, !hoveredOverSidebar); }
     }
 
     function makeLink(h) {
-      var link = document.createElement('li')
-      var text = h.textContent.replace(/\(.*\)$/, '')
+      const link = document.createElement('li');
+      const text = h.textContent.replace(/\(.*\)$/, '');
       link.innerHTML =
-        '<a class="section-link" data-scroll href="#' + h.id + '">' +
-          htmlEscape(text) +
-        '</a>'
-      return link
+        `<a class="section-link" data-scroll href="#${h.id}">${
+          htmlEscape(text)
+        }</a>`;
+      return link;
     }
 
-    function htmlEscape (text) {
+    function htmlEscape(text) {
       return text
         .replace(/&/g, '&amp;')
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&#39;')
         .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
+        .replace(/>/g, '&gt;');
     }
 
-    function collectH3s (h) {
-      var h3s = []
-      var next = h.nextSibling
+    function collectH3s(h) {
+      const h3s = [];
+      let next = h.nextSibling;
       while (next && next.tagName !== 'H2') {
         if (next.tagName === 'H3') {
-          h3s.push(next)
+          h3s.push(next);
         }
-        next = next.nextSibling
+        next = next.nextSibling;
       }
-      return h3s
+      return h3s;
     }
 
     function makeSubLinks(h3s, small) {
-      var container = document.createElement('ul')
+      const container = document.createElement('ul');
       if (small) {
-        container.className = 'menu-sub'
+        container.className = 'menu-sub';
       }
-      h3s.forEach(function(h) {
-        container.appendChild(makeLink(h))
-      })
-      return container
+      h3s.forEach((h) => {
+        container.appendChild(makeLink(h));
+      });
+      return container;
     }
 
     function setActive(id, shouldScrollIntoView) {
-      var previousActive = sidebar.querySelector('.section-link.active')
-      var currentActive = typeof id === 'string' ?
-        sidebar.querySelector('.section-link[href="#' + id + '"]') :
-        id
+      const previousActive = sidebar.querySelector('.section-link.active');
+      const currentActive = typeof id === 'string' ?
+        sidebar.querySelector(`.section-link[href="#${id}"]`) :
+        id;
       if (currentActive !== previousActive) {
-        if (previousActive) previousActive.classList.remove('active')
-        currentActive.classList.add('active')
+        if (previousActive) previousActive.classList.remove('active');
+        currentActive.classList.add('active');
         if (shouldScrollIntoView) {
-          var currentPageOffset = currentPageAnchor ?
+          const currentPageOffset = currentPageAnchor ?
             currentPageAnchor.offsetTop - 8 :
-            0
-          var currentActiveOffset = currentActive.offsetTop + currentActive.parentNode.clientHeight
-          var sidebarHeight = sidebar.clientHeight
-          var currentActiveIsInView = (
+            0;
+          const currentActiveOffset = currentActive.offsetTop + currentActive.parentNode.clientHeight;
+          const sidebarHeight = sidebar.clientHeight;
+          const currentActiveIsInView = (
             currentActive.offsetTop >= sidebar.scrollTop &&
             currentActiveOffset <= sidebar.scrollTop + sidebarHeight
-          )
-          var linkNotFurtherThanSidebarHeight = currentActiveOffset - currentPageOffset < sidebarHeight
-          var newScrollTop = currentActiveIsInView ?
+          );
+          const linkNotFurtherThanSidebarHeight = currentActiveOffset - currentPageOffset < sidebarHeight;
+          const newScrollTop = currentActiveIsInView ?
             sidebar.scrollTop :
             linkNotFurtherThanSidebarHeight ?
             currentPageOffset :
-            currentActiveOffset - sidebarHeight
-          sidebar.scrollTop = newScrollTop
+            currentActiveOffset - sidebarHeight;
+          sidebar.scrollTop = newScrollTop;
         }
       }
     }
 
     function makeHeaderClickable(link) {
-      var wrapper = document.createElement('a')
-      wrapper.href = '#' + link.id
-      wrapper.setAttribute('data-scroll', '')
-      link.parentNode.insertBefore(wrapper, link)
-      wrapper.appendChild(link)
+      const wrapper = document.createElement('a');
+      wrapper.href = `#${link.id}`;
+      wrapper.setAttribute('data-scroll', '');
+      link.parentNode.insertBefore(wrapper, link);
+      wrapper.appendChild(link);
     }
   }
-})()
+}());
